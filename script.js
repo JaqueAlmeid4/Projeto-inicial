8b5f2151d5b5409eb7258a38c7f4b3a163ee3763
JaqueAlmeida <jaque4990@gmail.com>
2026-04-12 23:28:55 -0300

    fix index and script mistakes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function toggleMode() {
      const img = document.querySelector("#profile img")
 
      //substituir a imagem
-    if(html.classList.contains('light')) {
+    if (html.classList.contains('light')) {
     img.setAttribute("src", "./assets/avatar-light.png")
     } else {
     img.setAttribute('src', './assets/avatar.png')
